5b26caa67ef7e67bd92eb8b56083efb2abf678f1
arunk555 <arunkurusev@gmail.com>
2022-09-15 13:07:53 +0000

    latest changes

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {welcome, signup}=require("../controllers/user");
+const {welcome, registerctrl}=require("../controllers/user");
 router.get("/welcome",welcome);
 router.post("/signup",registerctrl);
 
