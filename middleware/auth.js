6627b882de7bc2bdf2246051137c44f5e8158058
arunk555 <arunkurusev@gmail.com>
2022-09-17 13:22:07 +0000

    latest changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,28 @@
 const jwt=require("jsonwebtoken");
 const verifytoken=(req,res,next)=>{
-
-    return next();
+  const token=(req.headers['authorization'])?req.headers['authorization'].split(" ")[1]:"";
+  if(!token) {
+    res.status(403).json({
+        success: "false",
+        error: {
+            statusCode: 403,
+            message: "Access token is missing or invalid"
+        }
+    });
+  }
+  try{
+    const decoded= jwt.verify(token,process.env.TOKEN_KEY);
+    req.user=decoded;
+  }catch(error){
+    res.status(400).json({
+        success: "false",
+        error: {
+            statusCode: 400,
+            message: "Access token is invalid"
+        }
+    });
+  }
+  return next();
 
 }
 
